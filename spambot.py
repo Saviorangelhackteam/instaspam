b767fdf56527366a00df5fc9ef5eac9dcf71d526
mr-bomba <54629378+mr-bomba@users.noreply.github.com>
2019-09-12 11:06:29 +0300

    Update spambot.py

--- spambot.py
+++ spambot.py
@@ -1,156 +1,161 @@
-from requests.sessions import Session
-from requests import get
-from random import choice
-from multiprocessing import Process
-from colorama import init,Style,Fore
+dan requests.sessions ithalat Oturumu
+dan istekleri içe olsun
+dan rastgele ithalat seçimi
+dan çoklu işlem ithalat Süreci
+dan Colorama ithalat init, Stil, Fore
 
-BANNER = """
-   ____  __   __  ____    _   _   _      __   __
-  / ___| \ \ / / | __ )  | | | | | |     \ \ / /
- | |      \ V /  |  _ \  | | | | | |      \ V / 
- | |___    | |   | |_) | | |_| | | |___    | |  
-  \____|   |_|   |____/   \___/  |_____|   |_|  
-                                                
+BANNER  =  "" "
+
+   _________              _____             _          _     ______________  
+  | ________)           -/ / \ \-          | |        | |   (______________)
+  | |                  -/ /   \ \-         | |        | |          | |      
+  | |________         -/ /     \ \-        | |________| |          | | 
+  |_________ |       -/ /_______\ \-       | |________| |          | | 
+           | |      -/ /_________\ \-      | |        | |          | |   
+   ________| |     -/ /           \ \-     | |        | |          | | 
+  (__________|    -/ /             \ \-    |_|        |_|          |_|   
+
+   
 """
 
-USER_AGENTS = ["Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0",
-"Mozilla/5.0 (Android 4.4; Tablet; rv:41.0) Gecko/41.0 Firefox/41.0",
-"Mozilla/5.0 (Windows NT x.y; rv:10.0) Gecko/20100101 Firefox/10.0",
-"Mozilla/5.0 (X11; Linux i686; rv:10.0) Gecko/20100101 Firefox/10.0",
-"Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0",
-"Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0"]
+USER_AGENTS  = [ " Mozilla / 5.0 (Android 4.4; Mobil; rv: 41.0) Gecko / 41.0 Firefox / 41.0 " ,
+" Mozilla / 5.0 (Android 4.4; Tablet; rv: 41.0) Gecko / 41.0 Firefox / 41.0 " ,
+" Mozilla / 5.0 (Windows NT xy; rv: 10.0) Gecko / 20100101 Firefox / 10.0 " ,
+" Mozilla / 5.0 (X11; Linux i686; rv: 10.0) Gecko / 20100101 Firefox / 10.0 " ,
+" Mozilla / 5.0 (X11; Linux x86_64; rv: 10.0) Gecko / 20100101 Firefox / 10.0 " ,
+" Mozilla / 5.0 (Android 4.4; Mobil; rv: 41.0) Gecko / 41.0 Firefox / 41.0 " ]
 
-USER_AGENT = choice(USER_AGENTS)
+USER_AGENT  = seçim ( USER_AGENTS )
 
-class Client:
-    def __init__(self,username,password,proxy):
-        self.ses = Session()
-        self.loggedIn = False
-        self.username = username
-        self.password = password
-        self.proxy = proxy
+sınıf  müşteri :
+    def  __init__ ( kişisel , kullanıcı adı , şifre , vekil ):
+        kendi kendine .ses = Oturum ()
+        self .loggedIn =  Yanlış
+        self .username = kullanıcı adı
+        kendi kendine .password = şifre
+        öz .proxy = vekil
     
-    def Login(self):
-        if self.loggedIn == True:
-            return None
+    def  Giriş ( kişisel ):
+        eğer  kendini .loggedIn ==  Doğru :
+            geri dönüş  Yok
         
         loginData = {
-            "password":self.password,
-            "username":self.username,
-            "queryParams":"{}"
+            " Parola " : kendini .password,
+            " username " : kişisel kullanıcı adı ,
+            " queryParams " : " {} "
         }
-        homePageResponse = self.ses.get("https://www.instagram.com/accounts/login/")
+        homePageResponse =  self .ses.get ( " https://www.instagram.com/accounts/login/ " )
         loginHeaders = {
-            "Accept":"*/*",
-            "Accept-Encoding":"gzip,deflate,br",
-            "Accept-Language":"en-US,en;q=0.5",
-            "Connection":"keep-alive",
-            "Content-Type":"application/x-www-form-urlencoded",
-            "Host":"www.instagram.com",
-            "Referer":"https://www.instagram.com/accounts/login/",
-            "X-Requested-With":"XMLHttpRequest",
-            "X-Instagram-AJAX":"1",
-            "User-Agent":USER_AGENT,
-            "X-CSRFToken":homePageResponse.cookies.get_dict()["csrftoken"],
+            " Kabul et " : " * / * " ,
+            " Kabul Kodlama " : " gzip, deflate, br " ,
+            " Kabul Dili " : " en-US, en; q = 0.5 " ,
+            " Bağlantı " : " canlı tutma " ,
+            " İçerik Türü " : " uygulama / x-www-form-urlencoded " ,
+            " Ana Bilgisayar " : " www.instagram.com " ,
+            " Yönlendiren " : " https://www.instagram.com/accounts/login/ " ,
+            " X-İstenen-İle " : " XMLHttpRequest " ,
+            " X-Instagram-AJAX " : " 1 " ,
+            " Kullanıcı Aracısı " : USER_AGENT ,
+            " X-CSRFToken " : homePageResponse.cookies.get_dict () [ " csrftoken " ],
         }
         loginCookies = {
-            "rur":"PRN",
-            "csrftoken":homePageResponse.cookies.get_dict()["csrftoken"],
-            "mcd":homePageResponse.cookies.get_dict()["mcd"],
-            "mid":homePageResponse.cookies.get_dict()["mid"]
+            " rur " : " PRN " ,
+            " csrftoken " : homePageResponse.cookies.get_dict () [ " csrftoken " ],
+            " mcd " : homePageResponse.cookies.get_dict () [ " mcd " ],
+            " mid " : homePageResponse.cookies.get_dict () [ " mid " ]
         }
-        self.ses.headers.update(loginHeaders)
-        self.ses.cookies.update(loginCookies)
+        kendini .ses.headers.update (loginHeaders)
+        self .ses.cookies.update (loginCookies)
 
-        loginPostResponse = self.ses.post("https://www.instagram.com/accounts/login/ajax/",data=loginData)
+        loginPostResponse =  self .ses.post ( " https://www.instagram.com/accounts/login/ajax/ " , data = loginData)
     
-        if loginPostResponse.status_code == 200 and loginPostResponse.json()["authenticated"] == True:
-            self.loggedIn = True
-            mainPageResponse = self.ses.get("https://www.instagram.com/")
-            self.ses.cookies.update(mainPageResponse.cookies)
+        eğer loginPostResponse.status_code ==  200  ve loginPostResponse.json () [ " doğrulanmış " ] ==  Doğru :
+            self .loggedIn =  Doğru
+            mainPageResponse =  self .ses.get ( " https://www.instagram.com/ " )
+            self .ses.cookies.update (mainPageResponse.cookies)
     
-    def Spam(self,username,userid):
-        if self.loggedIn == False:
-            return None   
+    def  Spam ( öz , kullanıcı adı , kullanıcı kimliği ):
+        eğer  öz. KupasıIn ==  Yanlış :
+            geri dönüş  Yok   
 
-        link = "https://www.instagram.com/" + username + "/"
-        profileGetResponse = self.ses.get(link)
-        self.ses.cookies.update(profileGetResponse.cookies)
+        link =  " https://www.instagram.com/ "  + kullanıcı adı +  " / "
+        profileGetResponse =  self .ses.get (bağlantı)
+        self .ses.cookies.update (profileGetResponse.cookies)
         spamHeaders = {
-            "Accept":"*/*",
-            "Accept-Encoding":"gzip,deflate,br",
-            "Accept-Language":"en-US,en;q=0.5",
-            "Connection":"keep-alive",
-            "Content-Type":"application/x-www-form-urlencoded",
-            "DNT":"1",
-            "Host":"www.instagram.com",
-            "X-Instagram-AJAX":"2",
-            "X-Requested-With":"XMLHttpRequest",
-            "Referer":link,
-            "User-Agent":USER_AGENT,
-            "X-CSRFToken":profileGetResponse.cookies.get_dict()["csrftoken"],
+            " Kabul et " : " * / * " ,
+            " Kabul Kodlama " : " gzip, deflate, br " ,
+            " Kabul Dili " : " en-US, en; q = 0.5 " ,
+            " Bağlantı " : " canlı tutma " ,
+            " İçerik Türü " : " uygulama / x-www-form-urlencoded " ,
+            " DNT " : " 1 " ,
+            " Ana Bilgisayar " : " www.instagram.com " ,
+            " X-Instagram-AJAX " : " 2 " ,
+            " X-İstenen-İle " : " XMLHttpRequest " ,
+            " Yönlendirici " : link,
+            " Kullanıcı Aracısı " : USER_AGENT ,
+            " X-CSRFToken " : profileGetResponse.cookies.get_dict () [ " csrftoken " ],
         }
         spamData = {
-            "reason_id":"1",
-            "source_name":"profile"
+            " reason_id " : " 1 " ,
+            " kaynak_adı " : " profil "
         }
 
-        self.ses.headers.update(spamHeaders)
+        kendini .ses.headers.update (spamHeaders)
 
-        spamPostResponse = self.ses.post("https://www.instagram.com/users/"+ userid +"/report/",data=spamData)
-        if spamPostResponse.status_code == 200 and spamPostResponse.json()["description"] == "Your reports help keep our community free of spam.":
-            self.ses.close()
-            return True
-        else:
-            return False
+        spamPostResponse =  self .ses.post ( " https://www.instagram.com/users/ " + userid + " / report / " , data = spamData)
+        eğer spamPostResponse.status_code ==  200  ve spamPostResponse.json () [ " açıklama " ] ==  " Raporlarınız, topluluğumuzun spam'lerden korunmasına yardımcı olur. " :
+            kendini. ses.close ()
+            return  True
+        başka :
+             Yanlış döndürmek
 
-def Success(username,shit):
-    print(Fore.GREEN +"[" + username +"]" + Style.RESET_ALL
-    + " " + shit)
+def  Başarı ( kullanıcı adı , bok ):
+    print (Fore. YEŞİL  + " [ "  + kullanıcı adı + " ] "  + Stil. RESET_ALL
+    +  "  "  + bok
 
-def Fail(username,shit):
-    print(Fore.RED +"[" + username +"]" + Style.RESET_ALL
-    + " " + shit)
+def  Fail ( kullanıcı adı , bok ):
+    print (Fore. RED  + " [ "  + kullanıcı adı + " ] "  + Stil. RESET_ALL
+    +  "  "  + bok
 
-def Status(shit):
-    print(Fore.YELLOW +"[ İnsta Spam ]" + Style.RESET_ALL
-    + " " + shit)
+def  Durum ( bok ):
+    print (Fore. SARI  + " [İstenmeyen Posta Kodu] "  + Stil. RESET_ALL
+    +  "  "  + bok
 
-def DoitAnakin(reportedGuy,reportedGuyID,username,password,proxy):
-    try:
-        insta = None
-        if proxy != None:
-            insta = Client(username,password,None)
-        else:
-            insta = Client(username,password,None)
-        insta.Login()
-        result = insta.Spam(reportedGuy,reportedGuyID)
-        if insta.loggedIn == True and result == True:
-            Success(username,"Başarıyla SPAM atıldı!")
-        elif insta.loggedIn == True and result == False:
-            Fail(username,"Giriş başarılı ama SPAM atılması başarısız!")
-        elif insta.loggedIn == False:
-            Fail(username,"Giriş başarısız!")
-    except:
-        Fail(username,"Giriş yapılırken hata oluştu!")
+def  DoitAnakin ( reportedGuy , reportedGuyID , kullanıcı adı , şifre , vekil ):
+    dene :
+        insta =  Yok
+        eğer proxy ! =  Yok :
+            insta = Müşteri (kullanıcı adı, şifre, Yok )
+        başka :
+            insta = Müşteri (kullanıcı adı, şifre, Yok )
+        insta.Login ()
+        sonuç = insta.Spam (reportGuy, reportGuyID)
+        eğer insta.loggedIn ==  Doğru  ve sonuç ==  Doğru :
+            Success (kullanıcı adı, " Başarıyla SPAM atıldı! " )
+        elif insta.loggedIn ==  Doğru  ve sonuç ==  Yanlış :
+            Başarısız (kullanıcı adı, " Giriş başarılı ama SPAM atılması başarısız! " )
+        elif insta.loggedIn ==  Yanlış :
+            Başarısız (kullanıcı adı, " Giriş başarısız! " )
+    hariç :
+        Başarısız (kullanıcı adı, " Giriş yapıldıken hata oluştu! " )
 
-if __name__ == "__main__":
-    init()
-    userFile = open("userlist.txt","r")
+eğer  __name__  ==  " __main__ " :
+    içinde()
+    userFile =  open ( " userlist.txt " , " r " )
 
-    USERS = []
-    for user in userFile.readlines():
-        if user.replace("\n","").replace("\r","\n") != "":
-            USERS.append(user.replace("\n","").replace("\r","\n"))
+    KULLANICILAR  = []
+    userFile.readlines () içindeki kullanıcı için :
+        eğer user.replace ( " \ n " , " " ) .replace ( " \ r " , " \ n " ) ! =  " " :
+            USERS .append (user.replace ( " \ n " , " " )) .replace ( " \ r " , " \ n " ))
 
 
-    print(Fore.RED + BANNER + Style.RESET_ALL)
-    Status(str(len(USERS)) + " Adet Kullanıcı Yüklendi!\n")
-    reportedGuy = input(Fore.GREEN + "SPAM'lanacak Kişinin Kullanıcı Adı: " + Style.RESET_ALL)
-    reportedGuyID = input(Fore.GREEN + "SPAM'lanacak Kişinin User ID'si: " + Style.RESET_ALL)
-    print("")
-    Status("Saldırı başlatılıyor!\n")
+    print (Fore. RED  +  BANNER  + Stil. RESET_ALL )
+    Durum ( str ( len ( USERS )) +  " Adet Kullanıcı Yüklendi! \ N " )
+    reportedGuy =  girişi (. Ön YEŞİL  +  " SPAM'lanacak Kişinin Kullanıcı Adı: "  + . Stil RESET_ALL )
+    reportedGuyID =  input (Fore. YEŞİL  +  " SPAM'lanacak Kişinin Kullanıcı ID'si: "  + . Stil RESET_ALL )
+    Yazdır ( " " )
+    Durum ( " Saldırı başlatılıyor! \ N " )
 
-    for user in USERS:
-        p = Process(target=DoitAnakin,args=(reportedGuy,reportedGuyID,user.split(" ")[0],user.split(" ")[1],None))
-        p.start()
+    için kullanıcı içinde  KULLANICILAR :
+        p = Süreç ( target = DoitAnakin, args = ( işlem gördüGuy, reportGuyID, user.split ( "  " ) [ 0 ], user.split ( "  " ) [ 1 ], Yok ))
+        p.start ()
